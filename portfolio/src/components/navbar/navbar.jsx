d59365f59d06eed8968a0b9e64d9bda3c86ab772
PakornBank <pakorn22120@gmail.com>
2023-11-25 03:13:16 +0700

    Add navigation bar

---
+++ portfolio/src/components/navbar/navbar.jsx
@@ -0,0 +1,27 @@
+import './navbar.css';
+import { getImageUrl } from '../../utils.js';
+import { useState } from 'react';
+
+
+export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <nav className="navBar">
+            <a href="/">Portfolio</a>
+            <div className='menu'>
+                <img className='menuButton'
+                    src={menuOpen ? getImageUrl("nav/closeIcon.png")
+                        : getImageUrl("nav/menuIcon.png")}
+                    alt="menu-button"
+                    onClick={() => setMenuOpen(!menuOpen)} />
+                <ul className={`menuItems ${menuOpen && "menuOpen"}`}
+                onClick={() => setMenuOpen(false)}>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#education">Education</a></li>
+                    <li><a href="#experience">Experience</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>)
+}
