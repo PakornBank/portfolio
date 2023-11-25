1b8da5c25060f4fa22a4d823345eb9764eb0af34
PakornBank <pakorn22120@gmail.com>
2023-11-25 21:57:38 +0700

    Add experience section to portfolio.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Navbar from "./components/navbar/navbar";
 import Hero from "./components/hero/hero";
 import About from "./components/about/about";
+import Experience from "./components/experience/experience";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Hero></Hero>
         <hr />
         <About></About>
+        <hr />
+        <Experience></Experience>
       </main>
     </>
   );
---
+++ portfolio/src/components/experience/experience.jsx
@@ -0,0 +1,80 @@
+import { getImageUrl } from "../../utils";
+import "./experience.css";
+export default function Experience() {
+  return (
+    <div className="experience">
+      <h1>Experience</h1>
+      <div className="container">
+        <ul className="icons">
+          <li>
+            <div className="iconBackground">
+              <img src={getImageUrl("experience/html.png")} alt="" />
+            </div>
+
+            <h2>HTML</h2>
+          </li>
+          <li>
+            <div className="iconBackground">
+              <img src={getImageUrl("experience/css.png")} alt="" />
+            </div>
+
+            <h2>CSS</h2>
+          </li>
+          <li>
+            <div className="iconBackground">
+              <img src={getImageUrl("experience/react.png")} alt="" />
+            </div>
+
+            <h2>React</h2>
+          </li>
+          <li>
+            <div className="iconBackground">
+              <img src={getImageUrl("experience/node.png")} alt="" />
+            </div>
+
+            <h2>Node</h2>
+          </li>
+          <li>
+            <div className="iconBackground">
+              <img src={getImageUrl("experience/mongo.png")} alt="" />
+            </div>
+
+            <h2>MongoDB</h2>
+          </li>
+          <li>
+            <div className="iconBackground">
+              <img src={getImageUrl("experience/cpp.png")} alt="" />
+            </div>
+
+            <h2>C++</h2>
+          </li>
+          <li>
+            <div className="iconBackground">
+              <img src={getImageUrl("experience/c.png")} alt="" />
+            </div>
+
+            <h2>C</h2>
+          </li>
+          <li>
+            <div className="iconBackground">
+              <img src={getImageUrl("experience/arduino.png")} alt="" />
+            </div>
+
+            <h2>Arduino</h2>
+          </li>
+        </ul>
+        <div className="cards">
+          <div className="card">
+            <img src={getImageUrl("experience/posn.jpg")} alt="" />
+            <h2>Computer Olympiad Camp</h2>
+            <h3>CMU, 2021</h3>
+            <ul>
+              <li>Attended 1st & 2nd camp.</li>
+              <li>Learned about algorithms and data structures.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
